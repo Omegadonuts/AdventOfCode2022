37728fc85dd772ebe27b55950445662fec70363f
Omegadonuts <96491107+Omegadonuts@users.noreply.github.com>
2022-12-03 11:18:12 +0100

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,41 @@
+example = '''A Y
+B X
+C Z'''
+
+def convertStr(inputstr):
+    outputstr = inputstr.replace('X', 'A')
+    outputstr = outputstr.replace('Y', 'B')
+    outputstr = outputstr.replace('Z', 'C')
+    outputstr = outputstr.replace('A', 'Rock')
+    outputstr = outputstr.replace('B','Paper')
+    outputstr = outputstr.replace('C', 'Scissors')
+    outputlist = outputstr.split('\n')
+    
+    return outputlist
+
+def CalcRound(picks):
+    me = picks[1]
+    opponent = picks[0]
+    points = 0
+    if me == 'Rock':
+        points += 1
+        if opponent == 'Scissors':
+            points += 6
+    if me =='Paper':
+        points +=2
+        if opponent == 'Rock':
+            points += 6
+    if me == 'Scissors':
+        points += 3
+        if opponent == 'Paper':
+            points += 6
+    if me == opponent:
+        points +=3
+    return points
+
+resultpoints = 0
+pairslist = convertStr(example)
+for pairs in pairslist:
+    roundpoints = CalcRound(pairs.split())
+    resultpoints += roundpoints
+print(resultpoints)
